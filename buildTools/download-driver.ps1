3e494b04bf6f9a2a151a1220c2c1f3cdabec27a7
jsakamoto <junichi.jp@gmail.com>
2022-03-21 10:53:18 +0900

    Modernize the downloading webdriver file script

--- buildTools/download-driver.ps1
+++ buildTools/download-driver.ps1
@@ -6,32 +6,19 @@ $downloadurl = "https://github.com/SeleniumHQ/selenium/releases/download/seleniu
 
 # move current folder to where contains this .ps1 script file.
 $scriptDir = Split-Path $MyInvocation.MyCommand.Path
-pushd $scriptDir
-cd ..
-
+Push-Location $scriptDir
+Set-Location ..
 $currentPath = Convert-Path "."
 $downloadDir = Join-Path $currentPath "downloads"
+
 $zipPath = Join-Path $downloadDir $zipName
 $driverPath = Join-Path $downloadDir $driverName
 
 # download driver .zip file if not exists.
 if (-not (Test-Path $zipPath)) {
-    (New-Object Net.WebClient).Downloadfile($downloadurl, $zipPath)
-    if (Test-Path $driverPath) {
-        del $driverPath 
-    }
+    Invoke-WebRequest -Uri $downloadurl -OutFile $zipPath
 }
 
-# Decompress .zip file to extract driver .exe file.
-if (-not (Test-Path $driverPath)) {
-    $shell = New-Object -com Shell.Application
-    $zipFile = $shell.NameSpace($zipPath)
-
-    $zipFile.Items() | `
-        where { (Split-Path $_.Path -Leaf) -eq $driverName } | `
-        foreach {
-        $extractTo = $shell.NameSpace($downloadDir)
-        $extractTo.copyhere($_.Path)
-    }
-    sleep(2)
-}
+# Decompress .zip file to extract driver file.
+if (Test-Path $driverPath) { Remove-Item  $driverPath }
+Expand-Archive $zipPath $downloadDir
