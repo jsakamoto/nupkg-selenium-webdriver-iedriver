f3a2eda0302501da9a4c62211196eca777f2b4c7
jsakamoto <junichi.jp@gmail.com>
2022-09-29 07:40:53 +0900

    Upgrade to 4.5.0

--- buildTools/download-driver.ps1
+++ buildTools/download-driver.ps1
@@ -1,5 +1,5 @@
 # constants
-$version = "4.3.0"
+$version = "4.5.0"
 $driverName = "IEDriverServer.exe"
 $zipName = "IEDriverServer_Win32_$version.zip"
 $downloadurl = "https://github.com/SeleniumHQ/selenium/releases/download/selenium-$version/$zipName"
